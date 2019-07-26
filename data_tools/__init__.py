0fb6e500edffa92f4b59e592ec60ec83b22d81ba
Nan Li <nanli-7@users.noreply.github.com>
2019-07-26 09:38:01 -0400

    reorganize folder

--- data_tools/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__author__ = """Nan Li"""
-__email__ = 'xyz@examples.com'
-__version__ = '0.1.0'
--- docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use data_tools in a project::
-
-    import data_tools
